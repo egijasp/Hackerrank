5c80f3d660251d80cb9da6f387d0d70cff064df6
Egija Sprukule <egija.sprukule@expect.digital>
2022-06-14 01:02:35 +0300

    improve function

--- between-two-sets.ts
+++ between-two-sets.ts
@@ -1,11 +1,11 @@
 export function getTotal(a: number[], b: number[]): number {
-  let numbers = 0;
+  let count = 0;
 
-  for (let i = 0; i <= 100; i++) {
+  for (let i = a[0]; i <= b[0]; i++) {
     if (a.every((num) => i % num === 0) && b.every((num) => num % i === 0)) {
-      numbers++;
+      count++;
     }
   }
 
-  return numbers;
+  return count;
 }
