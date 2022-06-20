c86db4187998c81f644a3b66fa0c59de3a522538
Egija Sprukule <egija.sprukule@expect.digital>
2022-06-20 14:08:32 +0300

    update

--- sock-merchant.ts
+++ sock-merchant.ts
@@ -1,18 +1,15 @@
-interface Color {
-  [key: number]: number;
-}
-
-export function sockMerchant(ar: number[]): number {
-  const colorsCount = ar.reduce((allColors: Color, color) => {
+export function sockMerchant(ar: number[]) {
+  const colorsCount = ar.reduce((allColors, color) => {
     if (allColors[color]) {
       allColors[color]++;
       return allColors;
     }
     allColors[color] = 1;
     return allColors;
-  }, {});
+  }, {} as Record<string, number>);
 
-  return Object.values(colorsCount).reduce((count, value) => {
-    return count + Math.floor(value / 2);
-  }, 0);
+  return Object.values(colorsCount).reduce(
+    (count, value) => count + Math.floor(value / 2),
+    0
+  );
 }
