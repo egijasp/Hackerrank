f12333f71e45c9e069129967a37bcce767807aa7
Egija Sprukule <egija.sprukule@expect.digital>
2022-06-27 14:20:01 +0300

    changed strings to numbers

--- magic-square-forming.ts
+++ magic-square-forming.ts
@@ -1,18 +1,17 @@
 export function formingMagicSquare(s: number[][]): number {
   const patterns = [
-    "816357492",
-    "618753294",
-    "492357816",
-    "294753618",
-    "834159672",
-    "438951276",
-    "672159834",
-    "276951438",
+    [8, 1, 6, 3, 5, 7, 4, 9, 2],
+    [6, 1, 8, 7, 5, 3, 2, 9, 4],
+    [4, 9, 2, 3, 5, 7, 8, 1, 6],
+    [2, 9, 4, 7, 5, 3, 6, 1, 8],
+    [8, 3, 4, 1, 5, 9, 6, 7, 2],
+    [4, 3, 8, 9, 5, 1, 2, 7, 6],
+    [6, 7, 2, 1, 5, 9, 8, 3, 4],
+    [2, 7, 6, 9, 5, 1, 4, 3, 8],
   ];
 
   return patterns.reduce((lowest, pattern) => {
     const cost = pattern
-      .split("")
       .map((el) => Number(el))
       .reduce((sum, num, index) => {
         const row = Math.trunc(index / 3);
