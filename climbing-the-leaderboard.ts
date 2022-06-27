294851c05c066f2433e3226eab207c75fda5c544
Egija Sprukule <egija.sprukule@expect.digital>
2022-06-27 13:33:22 +0300

    with binary search

--- climbing-the-leaderboard.ts
+++ climbing-the-leaderboard.ts
@@ -4,16 +4,26 @@ export function climbingLeaderboard(
 ): number[] {
   const uniqueRank = [...new Set(ranked)];
 
-  let scores: number[] = [];
-
-  let i = uniqueRank.length - 1;
-
-  for (let score of player) {
-    while (score >= uniqueRank[i]) {
-      i--;
-    }
-
-    scores.push(i + 2);
-  }
-  return scores;
+  return player.reduce((rankedScore: number[], score) => {
+    const scoreIndex = binarySearch(uniqueRank, score);
+    rankedScore.push(scoreIndex + 1);
+    return rankedScore;
+  }, []);
 }
+
+const binarySearch = (searchArray: number[], score: number): number => {
+  let start = 0;
+  let end = searchArray.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    if (searchArray[mid] === score) {
+      return mid;
+    } else if (searchArray[mid] < score) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return start;
+};
