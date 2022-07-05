55629cb23dfc1e7041747747112bd391b965459b
Egija Sprukule <egija.sprukule@expect.digital>
2022-07-05 12:57:21 +0300

    fix typo

--- organizing-containers-of-balls.ts
+++ organizing-containers-of-balls.ts
@@ -2,15 +2,15 @@ export function organizingContainers(container: number[][]): string {
   if (container.length === 1 && container[0].length > 1) return 'Impossible'
 
   const row = new Array(container.length).fill(0)
-  const colon = new Array(container.length).fill(0)
+  const column = new Array(container.length).fill(0)
   for (let i = 0; i < container.length; i++) {
     for (let j = 0; j < container.length; j++) {
       row[i] += container[i][j]
-      colon[j] += container[i][j]
+      column[j] += container[i][j]
     }
   }
   const resultRow = row.sort().join()
-  const resultColon = colon.sort().join()
+  const resultColumn = column.sort().join()
 
-  return resultRow === resultColon ? 'Possible' : 'Impossible'
+  return resultRow === resultColumn ? 'Possible' : 'Impossible'
 }
